08e0163387c90c20606668d0f191562821306f92
nibankougen@gmail.com <kiyo_8into32@me.com>
2026-09-12 02:33:03 +0900

    ツールバースライドインアニメーションの追加

--- src/components/layout/EditorScreen.tsx
+++ src/components/layout/EditorScreen.tsx
@@ -158,7 +158,7 @@ export function EditorScreen() {
       <div className="flex flex-col h-screen">
         <Topbar />
         <div className="flex flex-1 min-h-0">
-          {!sidebarCollapsed && <Sidebar />}
+          <Sidebar collapsed={sidebarCollapsed} />
           <EditorPane mainInputRef={mainInputRef} />
         </div>
       </div>
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,11 +1,23 @@
 import { SceneList } from "../scenes/SceneList";
 import { CharacterList } from "../characters/CharacterList";
 
-export function Sidebar() {
+export function Sidebar({ collapsed }: { collapsed: boolean }) {
   return (
-    <div className="w-[230px] shrink-0 flex flex-col bg-panel border-r border-border">
-      <SceneList />
-      <CharacterList />
+    <div
+      className={
+        "shrink-0 overflow-hidden border-r border-border transition-[width] duration-200 ease-in-out" +
+        (collapsed ? " w-0 border-r-0" : " w-[230px]")
+      }
+    >
+      <div
+        className={
+          "w-[230px] h-full flex flex-col bg-panel transition-transform duration-200 ease-in-out" +
+          (collapsed ? " -translate-x-full" : " translate-x-0")
+        }
+      >
+        <SceneList />
+        <CharacterList />
+      </div>
     </div>
   );
 }
